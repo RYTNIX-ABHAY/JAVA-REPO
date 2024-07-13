6db80d0c7c58005f3c3537609836475614787a30
ABHAY PRATAP SINGH <abhaypratap12340000@gmail.com>
2024-07-13 21:36:25 +0530

    ok

---
+++ Shorting/Bubble.java
@@ -0,0 +1,30 @@
+package Shorting;
+
+import java.util.Arrays;
+
+public class Bubble {
+    
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+         bubble(arr);
+     System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean swapped;
+        for (int i = 0; i< arr.length;i++){
+            swapped=false;
+            for (int j= 1; j<=arr.length -i-1;j++){
+                if(arr[j-1]>arr[j]){
+                    // swap
+                    int t=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=t;
+                    swapped=true;
+                }
+
+            }if (!swapped){
+                break;
+            }
+        }
+    }
+}
