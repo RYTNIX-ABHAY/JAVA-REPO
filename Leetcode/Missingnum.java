1b3cf32d41cb1312f1a813a0a563e9f63aeb5e92
ABHAY PRATAP SINGH <abhaypratap12340000@gmail.com>
2024-07-20 21:35:18 +0530

    k

---
+++ Leetcode/Missingnum.java
@@ -0,0 +1,31 @@
+package Leetcode;
+
+public class Missingnum {
+    public static void main(String[] args) {
+        int[] arr = {4,0,2,1};
+        System.out.println(missing(arr));
+
+    }
+    static int missing(int [] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i];
+            if (arr[i]==arr[correct]){
+                i++;
+            }else{
+                swap(arr,i,correct);
+            }
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j){
+                return j;
+            }
+        }
+        return arr.length;
+    }
+    static  void swap(int[] arr, int f ,int s){
+        int t = arr[f];
+        arr[f]=arr[s];
+        arr[s]=t;
+    }
+}
